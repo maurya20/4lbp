b8a398cd31474baf8fd5d03effcdea106b8db029
maurya20 <mukesh.ice17@gmail.com>
2021-10-06 20:01:48 +0530

    changing the background color of the page corresponding to the button color.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import { myTheme } from "./theme";
 import { ThemeProvider } from "@mui/material";
 import Button from "@mui/material/Button";
-
+import { useState } from "react";
 function App() {
+  const [bg, setBg] = useState("#000000");
+  const bgStyle = {
+    background: bg,
+    height: "100vh",
+  };
   return (
     <ThemeProvider theme={myTheme}>
-      <Button variant="contained" color="primary">
-        primary
-      </Button>
-      <Button variant="contained" color="secondary">
-        secondary
-      </Button>
+      <div style={bgStyle}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setBg(myTheme.palette.primary.main)}
+        >
+          primary
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => setBg(myTheme.palette.secondary.main)}
+        >
+          secondary
+        </Button>
+        <Button
+          variant="contained"
+          color="negative"
+          onClick={() => setBg(myTheme.palette.negative.main)}
+        >
+          negative
+        </Button>
+        <Button
+          variant="contained"
+          color="pureblack"
+          onClick={() => setBg(myTheme.palette.pureblack.main)}
+        >
+          pure black
+        </Button>
+      </div>
     </ThemeProvider>
   );
 }
--- src/theme/index.js
+++ src/theme/index.js
@@ -3,10 +3,17 @@ import { createTheme } from "@mui/material/styles";
 export const myTheme = createTheme({
   palette: {
     primary: {
-      main: "#0052cc",
+      main: "#5E4FDB",
+      light: "#F3F2FC",
     },
     secondary: {
-      main: "#edf2ff",
+      main: "#1ABC9C",
+    },
+    negative: {
+      main: "#D64B4B",
+    },
+    pureblack: {
+      main: "#000000",
     },
   },
 });
